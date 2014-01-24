79f6d4db386e436537d2140969288c01580fbcc1
Tim McCall <tim.mccall@webfilings.com>
2014-01-23 21:17:59 -0800

    Fix Conversion Lint
    - Remove CHANGELOG
    - Update init.sh for consistency with uicomponents
    - Update wf-grunt reference so init script runs
    
    Fixes #2

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # colors
-ESC_SEQ="\x1b["
-COL_RED=$ESC_SEQ"31;01m"
-COL_CYAN=$ESC_SEQ"36;01m"
+COLOR_RED="\x1b[31m"
+COLOR_GREEN="\x1b[32m"
+COLOR_RESET="\x1b[0m"
 
 # prepare the dev environment
 git clean -fxd
@@ -11,19 +11,20 @@ npm install
 bower install
 
 if [ $? -ne 0 ]; then
-  echo "$COL_RED"
+  echo "$COLOR_RED"
   echo "\nINIT FAILED!\n"
   exit 1
 fi
 
-echo "$COL_CYAN"
+echo "$COLOR_GREEN"
 echo "\nGO FORTH AND CONQUER\n"
 
 echo "At any time you may:"
 echo "--------------------"
 echo "grunt -h    # show available tasks."
 echo "grunt dev   # lint and test, open the project web site and watch."
-echo "grunt qa    # initialize your environment, check code quality and open the project web site."
+echo "grunt qa    # check code quality and open the project web site."
 echo ""
 
+echo "$COLOR_RESET"
 exit 0
