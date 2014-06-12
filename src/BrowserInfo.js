f38b031197074347b8c750662db2aa5f9b98542a
Lance Fisher <lance.fisher@webfilings.com>
2014-06-12 11:19:37 -0600

    Fix failing BrowserInfo test in Firefox

--- src/BrowserInfo.js
+++ src/BrowserInfo.js
@@ -196,7 +196,7 @@ define(function(require) {
         // browser names
         this.Browsers = {
             Chrome: 'Chrome',
-            FireFox: 'Firefox',
+            Firefox: 'Firefox',
             IE: 'MSIE',
             Opera: 'OPR',
             Safari: 'Safari'
