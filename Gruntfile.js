f6b55814a8717949cfa7de20b9c0f287b00193df
Tim McCall <tim.mccall@workiva.com>
2014-08-11 13:57:08 -0600

    SauceLabs Integration in CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@ module.exports = function(grunt) {
 
     require('wf-grunt').init(grunt, {
         options: {
+            sauceLabsCredentials: {
+                username: process.env.SAUCE_LABS_USERNAME,
+                accessKey: process.env.SAUCE_LABS_ACCESS_KEY
+            },
             requireConfig: {
                 paths: {
                     modernizr: 'bower_components/modernizr/modernizr',
