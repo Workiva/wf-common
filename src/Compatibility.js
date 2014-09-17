bf4c46305e6b191a4a33049cdccba51bdfc9e4e1
Claire Sarsam <claire.sarsam@webfilings.com>
2014-09-17 11:04:39 -0700

    Refactor base64 functions into a seperate file

--- src/Compatibility.js
+++ src/Compatibility.js
@@ -14,8 +14,10 @@
  * limitations under the License.
  */
 
-define(function() {
+define(function(require) {
     'use strict';
+
+    var base64 = require('wf-js-common/Base64');
 
     /**
      * Create a window.console object
@@ -64,64 +66,8 @@ define(function() {
         }
 
         if (typeof configuration.window.atob === 'undefined' || typeof configuration.window.btoa === 'undefined') {
-            var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-            var InvalidCharacterError = function (message) {
-                this.message = message;
-            };
-            InvalidCharacterError.prototype = new Error();
-            InvalidCharacterError.prototype.name = 'InvalidCharacterError';
-
-            (function () {
-                function atob ( input ) {
-                    var str = String(input).replace(/=+$/, '');
-                    if (str.length % 4 === 1) {
-                        throw new InvalidCharacterError('"atob" failed: The string to be decoded is not correctly encoded.');
-                    }
-                    for (
-                        // initialize result and counters
-                        var bc = 0, bs, buffer, idx = 0, output = '';
-                        // get next character
-                        !!(buffer = str.charAt(idx++));
-                        // character found in table? initialize bit storage and add its ascii value;
-                        ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
-                        // and if not first of each 4 characters,
-                        // convert the first 8 bits to one ascii character
-                        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
-                    ) {
-                        // try to find character in table (0-63, not found => -1)
-                        buffer = chars.indexOf(buffer);
-                    }
-                    return output;
-                }
-
-                configuration.window.atob = atob;
-            })();
-
-            (function () {
-                function btoa ( input ) {
-                    var str = String(input);
-                    for (
-                        // initialize result and counter
-                        var block, charCode, idx = 0, map = chars, output = '';
-                        // if the next str index does not exist:
-                        //   change the mapping table to "="
-                        //   check if d has no fractional digits
-                        str.charAt(idx | 0) || (map = '=', idx % 1);
-                        // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
-                        output += map.charAt(63 & block >> 8 - idx % 1 * 8)
-                    ) {
-                        charCode = str.charCodeAt(idx += 3/4);
-                        if (charCode > 0xFF) {
-                            throw new InvalidCharacterError('"btoa" failed: The string to be encoded contains characters outside of the Latin1 range.');
-                        }
-                        block = block << 8 | charCode;
-                    }
-                    return output;
-                }
-
-                configuration.window.btoa = btoa;
-            })();
+            configuration.window.atob = base64.atob;
+            configuration.window.btoa = base64.btoa;
         }
     };
 
